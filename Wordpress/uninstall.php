f352c1d8bf0a855e4ac7edf85f02c3b97288652e
riiix-Biz <77607924+riiix-Biz@users.noreply.github.com>
2021-02-12 12:38:36 +0600

    Delete Wordpress directory

--- Wordpress/uninstall.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-defined('WP_UNINSTALL_PLUGIN') or exit();
-
-global $wpdb;
-
-// Delete plugin options
-$wpdb->query("DELETE FROM $wpdb->options WHERE option_name LIKE 'woocommerce_roskassa%';");
